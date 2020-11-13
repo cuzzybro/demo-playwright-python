28ea6561e99f69a10aa461be587350c248469dd9
LesJames <les.james@z.co.nz>
2020-11-14 10:54:25 +1300

    add comments

--- apps/twitter/twitter.py
+++ apps/twitter/twitter.py
@@ -1,7 +1,11 @@
 from playwright.sync_api import Page, Browser
+# Use this main file as a starting point and import the pages into this file to extend as below
+# There is no need to use the import in this file but it makes them available
 from apps.twitter.functions import twitter_login_page as login_page
 
 
+# this function optionally takes in either a Page or Browser object from the playwright sync api
+# it also return the page obect that is either passed or built in this function
 def launch(page: Page = None, browser: Browser = None):
 
     if browser is not None:
