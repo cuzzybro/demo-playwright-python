2fa7d6f34aa0f211e1dd59ae86602907efa2628c
LesJames <les.james@z.co.nz>
2020-11-14 11:58:57 +1300

    .

--- apps/twitter/twitter.py
+++ apps/twitter/twitter.py
@@ -5,7 +5,7 @@ from apps.twitter.functions import twitter_login_page as login_page
 
 
 # this function optionally takes in either a Page or Browser object from the playwright sync api
-# it also return the page obect that is either passed or built in this function
+# it also return the page object that is either passed or built in this function
 def launch(page: Page = None, browser: Browser = None):
     if browser is not None:
         page = browser.newPage()
